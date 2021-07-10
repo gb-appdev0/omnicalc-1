4d10f8b02fbbc2563e14c40081d322b01c47fcf8
gb-appdev <sbhaktha@chicagobooth.edu>
2021-07-10 03:06:54 +0000

    finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,13 @@ class ApplicationController < ActionController::Base
     end
 
     def payment_results
-        @num = params.fetch("user_square").to_f
+        @apr = (params.fetch("apr").to_f).round(4)
+        @years = params.fetch("years").to_f
+        @principal = params.fetch("principal").to_f
+        @payment = (@apr/100/12.0*@principal/(1.0 - (1.0 + @apr/100/12.0)**(-@years*12.0))).to_s(:currency)
+        @pv = @principal.to_s(:currency)
+        #@perc = @apr.to_s(:percentage)
+        @perc = sprintf('%.4f%%', @apr)
         render({ :template => "game_templates/payment_results.html.erb" })
 
     end
@@ -35,7 +41,9 @@ class ApplicationController < ActionController::Base
     end
 
     def random_results
-        @num = params.fetch("user_square").to_f
+        @min = params.fetch("min").to_f
+        @max = params.fetch("max").to_f
+        @random = rand(@min..@max)
         render({ :template => "game_templates/random_results.html.erb" })
 
     end
